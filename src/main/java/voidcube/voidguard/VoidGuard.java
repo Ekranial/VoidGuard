01b2a1142a9c76283d8186f8d9d74776a5d25de4
Ekran1al <Ekranial@yandex.ru>
2023-07-24 16:15:53 +0300

    1st

---
+++ src/main/java/voidcube/voidguard/VoidGuard.java
@@ -0,0 +1,17 @@
+package voidcube.voidguard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
